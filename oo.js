247c8d3684f10bd4f58775d2cad8ac39ab308045
btwSanToS <andrelmsantos.87@gmail.com>
2024-04-17 19:33:27 -0300

    Aula inicial Objeto

---
+++ oo.js
@@ -0,0 +1,25 @@
+// Iniciando Aulas Objeto
+// CÓDIGO NÃO EXECUTÁVEL!!!!!
+
+// Procedural
+processamento(valor1, valor2, valor3) // Valores a serem processados.
+/* 
+O código acima é apenas um exemplo de um procedimento a manipular valores.
+Ao fim, ele retornará o valor processado.
+*/
+
+/* Orientado a Objeto
+A linguagem orientada, o programa é estruturado em torno de objetos, que são instâncias de classes. 
+Cada objeto possui características (atributos) e comportamentos (métodos) associados a ele.
+
+*/
+objeto = {   // Atributos ou Propriedades ↓↓↓↓
+    valor1,
+    valor2,
+    valor3,
+    processamento() { // ←←←← Método 
+        //...
+    }
+}
+
+objeto.processamento() // Processamento a ser utilizado, apontando para o Objeto.
