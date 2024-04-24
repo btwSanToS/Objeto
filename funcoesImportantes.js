720c61405d2fbe5c2a5a7d0f07f2d79498c28a9f
btwSanToS <andrelmsantos.87@gmail.com>
2024-04-24 13:53:42 -0300

    objeto com funcoes importantes

---
+++ funcoesImportantes.js
@@ -0,0 +1,9 @@
+const pessoa = {
+    nome: 'André',
+    idade: 23,
+    peso: 85
+}
+
+console.log(Object.keys(pessoa)) // Retornará as chaves do Objeto;
+console.log(Object.values(pessoa)) // Retornará os valores do Objeto;
+console.log(Object.entries(pessoa)) // Retornará um array com chave e valor, separados por dupla;
