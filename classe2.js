9f421b2c1ae5638fb8db8196d83f216af9f6c96d
btwSanToS <andrelmsantos.87@gmail.com>
2024-05-02 18:33:26 -0300

    criação classe carro

--- classe2.js
+++ classe2.js
@@ -1,21 +1,48 @@
 class Avo {
-    constructor(sobrenome){
+    constructor(sobrenome) {
         this.sobrenome = sobrenome
     }
 }
 
 class Pai extends Avo {
-    constructor(sobrenome, profissão = 'Professor'){
+    constructor(sobrenome, profissão = 'Professor') {
         super(sobrenome)
-            this.profissão = profissão
-        }
+        this.profissão = profissão
     }
+}
 
 class Filho extends Pai {
-    constructor(){
+    constructor() {
         super('Silva')
     }
 }
 
 const filho = new Filho
 console.log(filho)
+
+// Testes ↓↓↓↓
+// Classe carro.
+
+class Carro {
+    constructor(marca, modelo) {
+        this.marca = marca
+        this.modelo = modelo
+    }
+
+    acelerar() {
+        console.log(`O ${this.marca} ${this.modelo} está acelerando!`)
+    }
+
+    frear() {
+        console.log(`O ${this.marca} ${this.modelo} está freando!`)
+    }
+}
+
+//Criar o veículo com base na classe;
+const carro1 = new Carro('Honda', 'Civic');
+carro1.acelerar()
+carro1.frear()
+
+const carro2 = new Carro ('Mitsubishi', 'Lancer')
+carro2.acelerar()
+carro2.frear()
